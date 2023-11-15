60b4af41587bfc01d4215b40f54a913289cacd66
André Luis Lui <andre.lui00@gmail.com>
2023-11-14 21:29:52 -0300

    Create first script (test.py) to set GPIO ports

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # FSAE Eletronic Project
+
+Projeto destinado a criar uma arquitetura de aquisição de dados baseada no projeto do EESC-USP Formula SAE [EX](https://formula.eesc.usp.br/ex/), utilizando Rasperry Pi.
+
+## Roadmap
+
+* Piscar led na placa
+* Piscar led utilizando inputs
+* GPS
+* Real Time Clock
+* CAN
+
+## Versão dos Hardwares
+
+* Raspberry Pi 3 Model B V1.2
+* Raspberry Pi Zero W V1.1
+
+## Instalação
+
+1. Baixar o Raspberry Pi Imager (Raspberry Pi Imager) usando o SNAP (`sudo snap install rpi-imager`);
+
---
+++ test.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
+from time import sleep     # Import the sleep function from the time module
+GPIO.setwarnings(False)    # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+GPIO.setup(40, GPIO.OUT, initial=GPIO.LOW) 
+
+while True: # Run forever
+    GPIO.output(40, GPIO.HIGH) # Turn on
+    sleep(1)                  # Sleep for 1 second
+    GPIO.output(40, GPIO.LOW)  # Turn off
+    sleep(1)                  # Sleep for 1 second
+
